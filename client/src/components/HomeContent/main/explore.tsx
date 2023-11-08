69c710cb83ba4b10744993e6f619ce4f06d7cdc3
tomaszsliwakowski <tomek08082002@gmail.com>
2023-11-08 19:17:39 +0100

    Changes to be committed: 	modified:   client/src/components/HomeContent/main/explore.tsx 	modified:   client/src/components/HomeContent/main/main.module.css

--- client/src/components/HomeContent/main/explore.tsx
+++ client/src/components/HomeContent/main/explore.tsx
@@ -1,47 +1,35 @@
 import styles from "./main.module.css";
 import { Link } from "react-router-dom";
 
+type EXPLORE_TYPE = {
+  address: string;
+  img: string;
+  name: string;
+};
+
+const exploreData: EXPLORE_TYPE[] = [
+  { address: "/", img: "./food.jpg", name: "Food" },
+  { address: "/", img: "./travel.jpg", name: "Travel" },
+  { address: "/", img: "./lifestyle.jpg", name: "LifeStyle" },
+  { address: "/", img: "./fashion.jpg", name: "Fashion" },
+  { address: "/", img: "./buisness.jpg", name: "Buisness" },
+  { address: "/", img: "./sports.jpg", name: "Sports" },
+  { address: "/", img: "./cars.jpg", name: "Cars" },
+  { address: "/", img: "./fitness.jpg", name: "Fitness" },
+  { address: "/", img: "./animals.jpg", name: "Animals" },
+];
+
 export default function Explore() {
   return (
     <div className={styles.main__explore}>
       <h2>Explore Popular Categories</h2>
       <ul>
-        <Link to={"/"} className={styles.main__category}>
-          <span>Food</span>
-          <img src="./food.jpg" alt="food" />
-        </Link>
-        <Link to={"/"} className={styles.main__category}>
-          <span>Travel</span>
-          <img src="./travel.jpg" alt="travel" />
-        </Link>
-        <Link to={"/"} className={styles.main__category}>
-          <span>LifeStyle</span>
-          <img src="./lifestyle.jpg" alt="lifeStyle" />
-        </Link>
-        <Link to={"/"} className={styles.main__category}>
-          <span>Fashion</span>
-          <img src="./fashion.jpg" alt="fashion" />
-        </Link>
-        <Link to={"/"} className={styles.main__category}>
-          <span>Buisness</span>
-          <img src="./buisness.jpg" alt="buisness" />
-        </Link>
-        <Link to={"/"} className={styles.main__category}>
-          <span>Sports</span>
-          <img src="./sports.jpg" alt="sports" />
-        </Link>
-        <Link to={"/"} className={styles.main__category}>
-          <span>Cars</span>
-          <img src="./cars.jpg" alt="cars" />
-        </Link>
-        <Link to={"/"} className={styles.main__category}>
-          <span>Fitness</span>
-          <img src="./fitness.jpg" alt="fitness" />
-        </Link>
-        <Link to={"/"} className={styles.main__category}>
-          <span>Animals</span>
-          <img src="./animals.jpg" alt="animals" />
-        </Link>
+        {exploreData.map((item: EXPLORE_TYPE, id: number) => (
+          <Link key={id} to={item.address} className={styles.main__category}>
+            <span>{item.name}</span>
+            <img src={item.img} alt={item.name} />
+          </Link>
+        ))}
       </ul>
     </div>
   );
