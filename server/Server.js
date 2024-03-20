d747e19cc3810805c42058aa515e62600c32fd66
tomaszsliwakowski <tomek08082002@gmail.com>
2024-03-20 11:14:14 +0100

    Changes to be committed: 	modified:   server/Server.js

--- server/Server.js
+++ server/Server.js
@@ -16,7 +16,10 @@ const port = process.env.PORT;
 app.use(body_parser_1.default.json({ limit: "50mb" }));
 app.use(body_parser_1.default.urlencoded({ limit: "50mb", extended: true }));
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({ credentials: true, origin: "https://social-book-bay.vercel.app/" }));
+app.use((0, cors_1.default)({
+    credentials: true,
+    origin: "https://social-book-bay.vercel.app/",
+}));
 app.use("/graphql", (0, express_graphql_1.graphqlHTTP)({
     schema: schema_1.schema,
     graphiql: true,
