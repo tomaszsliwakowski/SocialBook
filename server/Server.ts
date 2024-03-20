8b8a7f49da31f1dd911e3760e650f23eb529496a
tomaszsliwakowski <tomek08082002@gmail.com>
2024-03-20 11:22:30 +0100

    Changes to be committed: 	modified:   server/Server.ts
     Changes not staged for commit:
    	modified:   server/Server.js

--- server/Server.ts
+++ server/Server.ts
@@ -15,12 +15,20 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-    origin: "https://social-book-bay.vercel.app/",
-  })
-);
+//app.use(
+// cors({
+//  credentials: true,
+//   origin: "https://social-book-bay.vercel.app/",
+// })
+//);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(
   "/graphql",
