921ed02e2b68bd97b180c9a146d0215444eec252
tomaszsliwakowski <tomek08082002@gmail.com>
2024-03-20 13:22:08 +0100

    Changes to be committed: 	modified:   server/Server.js

--- server/Server.js
+++ server/Server.js
@@ -18,10 +18,10 @@ app.use(body_parser_1.default.urlencoded({ limit: "50mb", extended: true }));
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
     credentials: true,
-    origin: "https://social-book-bay.vercel.app/",
+    origin: "https://tomaszsliwakowski.github.io/SocialBook/",
 }));
 app.use("/graphql", (0, cors_1.default)({
-    origin: "https://social-book-bay.vercel.app/",
+    origin: "https://tomaszsliwakowski.github.io/SocialBook/",
     credentials: true,
 }), (0, express_graphql_1.graphqlHTTP)({
     schema: schema_1.schema,
